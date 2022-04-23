7db236bcd219e12cc4b0288d67f22cec7cd445f6
Felix Ivander <felix.ganumba@gmail.com>
2022-04-23 23:49:14 +0700

    Finished C1W4 Assignment

--- C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
+++ C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
@@ -25,20 +25,22 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
       // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({units: 512, activation: "relu"}),
+      tf.layers.dense({units: 256, activation: "relu"}),
+      tf.layers.dense({units: 5, activation: "softmax"}),
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);// YOUR CODE HERE
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'})// YOUR CODE HERE);
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -76,8 +78,10 @@ function handleButton(elem){
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE
-		
-            
+		case "4":
+      lizardSamples++;
+      document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+      break;
 	}
 	label = parseInt(elem.id);
 	const img = webcam.capture();
@@ -113,7 +117,9 @@ async function predict() {
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE 
-	
+    case 4:
+      predictionText = "I see Lizard";
+      break;
             
 	}
 	document.getElementById("prediction").innerText = predictionText;
